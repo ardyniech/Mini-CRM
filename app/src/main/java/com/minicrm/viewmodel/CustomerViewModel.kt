687b3fa327ecf3476412deba7a659e01cfea154d
ardyniech <54908304+ardyniech@users.noreply.github.com>
2026-09-08 11:07:13 +0700

    feat: implement solid MVVM foundation for Mini-CRM (app/src/main/java/com/minicrm/viewmodel/CustomerViewModel.kt)

--- app/src/main/java/com/minicrm/viewmodel/CustomerViewModel.kt
+++ app/src/main/java/com/minicrm/viewmodel/CustomerViewModel.kt
@@ -1,15 +1,11 @@
 package com.minicrm.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.MutableLiveData
 import com.minicrm.data.CustomerRepository
 import com.minicrm.model.Customer
 
 class CustomerViewModel(private val repository: CustomerRepository) : ViewModel() {
-    private val _customers = MutableLiveData<List<Customer>>()
-    val customers: MutableLiveData<List<Customer>> = _customers
-
-    fun loadCustomers() {
-        _customers.value = repository.getCustomers()
+    fun fetchCustomers(): List<Customer> {
+        return repository.getCustomers()
     }
 }
